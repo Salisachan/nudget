b6c072e202e5e3442678130586b3f5ff30628a7b
Salisa Chanchokpong <schanchokpong@gmail.com>
2026-05-03 20:05:23 -0700

    changed main.jsx and app.jsx create react-router

---
+++ client/src/context/AuthContext.jsx
@@ -0,0 +1,30 @@
+import { createContext, useContext, useState } from 'react';
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [token, setToken] = useState(localStorage.getItem('token') || null);
+    const [userId, setUserId] = useState(localStorage.getItem('userId') || null);
+
+    const login = (token, userId) => {
+        localStorage.setItem('token', token);
+        localStorage.setItem('userId', userId);
+        setToken(token);
+        setUserId(userId);
+    };
+
+    const logout = () => {
+        localStorage.removeItem('token');
+        localStorage.removeItem('userId');
+        setToken(null);
+        setUserId(null);
+    };
+
+    return (
+        <AuthContext.Provider value={{ token, userId, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export const useAuth = () => useContext(AuthContext);
